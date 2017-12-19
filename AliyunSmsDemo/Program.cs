494da2c389aa93e2b6e08b77039a21750bb72895
Gavin Fang <fangshanzhang@live.com>
2017-12-19 15:44:39 +0800

    Update Demo

--- AliyunSmsDemo/Program.cs
+++ AliyunSmsDemo/Program.cs
@@ -2,42 +2,45 @@
 using Aliyun.Acs.Core.Profile;
 using Aliyun.Acs.Sms.Model.V20170525;
 using System;
+using System.Threading.Tasks;
 
 namespace AliyunSmsDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            //以下字段换成自己的
             string regionId = "cn-hangzhou";
             string accessKeyId = "xxxxxxxx";
             string accessKeySecret = "xxxxxxxxxxxxxxxxxxxxxxxx";
             string signName = "xxxxxxxxxx";
             string phoneNumbers = "15xxxxxxxxx";
             string templateCode = "SMS_73985016";
-            string templateParam = "{\"code\":\"123456\", \"product\":\"MyProduct\"}";
+            string templateParam = "{\"code\":\"123456\"}";
 
 
             IClientProfile clientProfile = DefaultProfile.GetProfile(regionId, accessKeyId, accessKeySecret);
             DefaultProfile.AddEndpoint(regionId, regionId, "Dysmsapi", "dysmsapi.aliyuncs.com");
 
-            IAcsClient acsClient = new DefaultAcsClient(clientProfile);
+            IAsyncAcsClient acsClient = new DefaultAcsClient(clientProfile);
 
-            SendSmsRequest request = new SendSmsRequest();
-
-            request.SignName = signName;
-            request.TemplateCode = templateCode;
-            request.PhoneNumbers = phoneNumbers;
-            request.TemplateParam = templateParam;
+            var request = new SendSmsRequest
+            {
+                SignName = signName,
+                TemplateCode = templateCode,
+                PhoneNumbers = phoneNumbers,
+                TemplateParam = templateParam
+            };
 
             try
             {
-                SendSmsResponse response = acsClient.GetAcsResponse(request);
+                var response = await acsClient.GetAcsResponseAsync(request);
+
+                Console.WriteLine(response.Code);
             }
             catch (Exception ex)
             {
-                
+                Console.WriteLine(ex);
             }
         }
     }
